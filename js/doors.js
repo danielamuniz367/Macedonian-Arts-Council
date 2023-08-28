70c1fbdaafd98e48403e2e4a29e349203dffd2d4
Daniela Muniz <danielamuniz367@gmail.com>
2023-08-28 14:31:03 -0400

    added rest of door images and adjusted javascript

--- js/doors.js
+++ js/doors.js
@@ -1,14 +1,10 @@
-let hoverImages = document.getElementById("group1").getElementsByTagName("img");
-hoverImages = Array.from(hoverImages);
-console.log(hoverImages);
-
-let zoomedImage = document.getElementsByClassName("zoomed");
-zoomedImage = Array.from(zoomedImage).pop();
-console.log(zoomedImage.src);
-
-Array.from(hoverImages).forEach((hoverImage) => {
-  hoverImage.addEventListener("mouseover", (e) => {
-    let hoverTarget = e.currentTarget.src;
-    zoomedImage.src = hoverTarget;
-  });
-});
+Array.from(document.querySelectorAll(".thumbnails img")).forEach(
+  (hoverImage) => {
+    hoverImage.addEventListener("mouseover", (e) => {
+      let hoverTarget = e.currentTarget.src;
+      e.currentTarget.closest(
+        ".thumbnails"
+      ).previousSibling.previousSibling.src = hoverTarget;
+    });
+  }
+);
